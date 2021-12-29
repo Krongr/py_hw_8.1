b54a565c387c7ccd84fc5ea23dd727107e9a20ba
Artem Nazarov <krongr.study@ya.ru>
2021-12-29 15:36:38 +0300

    Fundamental changes in how it works

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import requests
 
 
-class Hero:
-    list_of_all = []
-
-    def __init__(self, name: str, token):
-        self.name = name.title()
-        self.intelligence = float(requests.get(f'https://superheroapi.com/api/{token}/search/{self.name}').json()['results'][0]['powerstats']['intelligence'])
-        self.list_of_all.append(self)
+def get_token_from_file(file_name='token.txt') -> str:
+    """Возвращает авторизационный токен сохраненный в отдельном файле 'file_name'."""
+    with open(file_name, 'rt', encoding='utf-8') as file:
+        return file.read().strip()
 
 
-def designate_the_smartest_hero(list_of_heroes=Hero.list_of_all):
-    """Определяет объект, обладающий самым высоким значением атрибута 'intelligence' среди элементов списка 'list_of_heroes'.
-    Элементами списка должны быть экземпляры класса 'Hero'.
-    """
-    highest_intelligence = 0
+def create_dict_of_heroes(list_of_heroes: list, stat: str) -> dict:
+    """Возвращает словарь, где ключи - имена героев из списка 'list_of_heroes', а значения - выбранная характеристика 'stat'."""
+    dict_of_heroes = {}
     for hero in list_of_heroes:
-        if hero.intelligence > highest_intelligence:
-            highest_intelligence = hero.intelligence
-            the_smartest_hero = hero.name
-    print(f'{the_smartest_hero} - умнейший герой')
+        dict_of_heroes[hero] = float(requests.get(f'https://superheroapi.com/api/{token}/search/{hero}').json()['results'][0]['powerstats'][stat])
+    return dict_of_heroes
+
+
+def designate_the_smartest_hero(list_of_heroes: list, stat='intelligence'):
+    """Определяет элемент словаря, полученный от функции 'create_dict_of_heroes()', обладающий самым высоким значением'."""
+    highest_intelligence = 0
+    dict_of_heroes = create_dict_of_heroes(list_of_heroes, stat)
+    for hero in dict_of_heroes:
+        if dict_of_heroes[hero] > highest_intelligence:
+            highest_intelligence = dict_of_heroes[hero]
+            the_smartest_hero = hero
+    print(f'{the_smartest_hero} - герой c самым высоким показателем {stat.title()}.')
 
 
 if __name__ == "__main__":
-    token = ''
+    token = get_token_from_file()
+    list_of_heroes = ['Hulk', 'Captain America', 'Thanos']
 
-    hulk = Hero('hulk', token)
-    captain_america = Hero('Captain America', token)
-    thanos = Hero('Thanos', token)
-
-    designate_the_smartest_hero()
+    designate_the_smartest_hero(list_of_heroes)
